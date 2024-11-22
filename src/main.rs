724e7bd5291d1352d96fa4eae6d10b4c316a0e47
Gavin Morrow <gavinfmorrow@gmail.com>
2024-11-22 17:50:39 -0500

    Allow accessing vars from outer scopes

--- src/main.rs
+++ src/main.rs
@@ -650,12 +650,28 @@ mod interperter {
                 self.values.insert(identifier, value);
             }
 
+            fn resolve_scope(&self, identifier: &Identifier) -> Option<Identifier> {
+                if self.values.contains_key(identifier) {
+                    Some(identifier.clone())
+                } else {
+                    let mut id = identifier.clone();
+                    while let Some(id) = id.in_parent_scope() {
+                        if let Some(resolved_ident) = self.resolve_scope(&id) {
+                            return Some(resolved_ident);
+                        }
+                    }
+                    None
+                }
+            }
+
             pub fn get(&self, identifier: &Identifier) -> Option<&Value> {
-                self.values.get(identifier)
+                self.resolve_scope(identifier)
+                    .and_then(|identifier| self.values.get(&identifier))
             }
 
             fn get_mut(&mut self, identifier: &Identifier) -> Option<&mut Value> {
-                self.values.get_mut(identifier)
+                self.resolve_scope(identifier)
+                    .and_then(|identifier| self.values.get_mut(&identifier))
             }
 
             /// Update a value if it exists.
@@ -672,20 +688,30 @@ mod interperter {
             }
         }
 
-        #[derive(Eq, PartialEq, Hash)]
+        #[derive(Clone, Eq, PartialEq, Hash)]
         pub struct Identifier {
             scope: Scope,
             name: String,
         }
         impl Identifier {
-            pub(crate) fn new(scope: Scope, name: String) -> Self {
+            pub fn new(scope: Scope, name: String) -> Self {
                 Self { scope, name }
+            }
+
+            pub fn in_parent_scope(&mut self) -> Option<Self> {
+                self.scope.parent.clone().map(|parent_scope| {
+                    *self = Identifier {
+                        scope: *parent_scope,
+                        name: self.name.clone(),
+                    };
+                    self.clone()
+                })
             }
         }
 
         #[derive(Clone, Eq, PartialEq, Hash)]
         pub struct Scope {
-            parent: Option<Box<Scope>>,
+            pub parent: Option<Box<Scope>>,
             id: ScopeId,
         }
         impl Scope {
